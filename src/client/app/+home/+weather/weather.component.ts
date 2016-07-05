77c6c0836bcdee6814856903aab0c55f17ffd8fe
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-07-05 11:32:45 -0400

    initial commit for jarvis dashboard

---
+++ src/client/app/+home/+weather/weather.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'l3-weather',
+    templateUrl: 'weather.component.html'
+})
+
+
+export class WeatherComponent {
+    constructor() {
+        console.log('Weather Component');
+    }
+
+}
